35a03af044b308c94d887b9d00e8a808b757dbaf
Betim Hodza <106614572+Betim-Hodza@users.noreply.github.com>
2024-05-08 10:32:04 -0500

    Update: back button works

--- App/app/src/main/java/com/example/app/MainActivity.kt
+++ App/app/src/main/java/com/example/app/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -22,6 +23,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
@@ -67,10 +70,10 @@ fun MomoNav() {
                     )
                 },
                 navigationIcon = {
-                    IconButton(onClick = {/*Do something*/}) {
+                    IconButton(onClick = {navigationController.navigateUp()}) {
                         Icon(
                             imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "Localized Desc."
+                            contentDescription = null
                         )
                     }
                 },
@@ -90,7 +93,7 @@ fun MomoNav() {
         NavHost(
             navController = navigationController,
             startDestination = Screens.Home.screen,
-            modifier = Modifier.padding(paddingValues)
+            modifier = Modifier.padding(paddingValues),
         ) {
             composable(Screens.Home.screen) { Home(navController = navigationController)}
             composable(Screens.About.screen) { About(navController = navigationController)}
