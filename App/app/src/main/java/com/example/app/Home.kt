263e319b145c11c7158418134eb5223f7453453d
Betim Hodza <106614572+Betim-Hodza@users.noreply.github.com>
2024-05-08 23:08:58 -0500

    Update: now have a app navigation drawer Todo: About page Officers page Events page Apps page

--- App/app/src/main/java/com/example/app/Home.kt
+++ App/app/src/main/java/com/example/app/Home.kt
@@ -6,41 +6,59 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 
 @Composable
 fun Home(navController: NavHostController, modifier: Modifier = Modifier) {
-    Column (
-        modifier = modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Image(
-            painter = painterResource(id = R.drawable.momo),
-            contentDescription = "Momologo",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        )
-        Text(
-            text = "Discover Magic of Making Apps!",
-            modifier = Modifier.padding(16.dp),
-            style = MaterialTheme.typography.headlineSmall
-        )
-        AppButtons(
-            onAboutClick = { navController.navigate(Screens.About.screen) },
-            onOfficerClick = { navController.navigate(Screens.Officers.screen) },
-            onEventsClick = { navController.navigate(Screens.Events.screen) },
-            onAppsClick = { navController.navigate(Screens.Apps.screen) },
-        )
+    Surface(
+        color = MaterialTheme.colorScheme.background
+    ) {
+        LazyColumn (
+            modifier = modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            item {
+                Image(
+                    painter = painterResource(id = R.drawable.momo),
+                    contentDescription = "Momologo",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+            }
+            item {
+                Text(
+                    text = "Discover Magic of Making Apps!",
+                    modifier = Modifier.padding(16.dp),
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            }
+            item {
+                AppButtons(
+                    onAboutClick = { navController.navigate(Screens.About.screen) },
+                    onOfficerClick = { navController.navigate(Screens.Officers.screen) },
+                    onEventsClick = { navController.navigate(Screens.Events.screen) },
+                    onAppsClick = { navController.navigate(Screens.Apps.screen) },
+                )
+            }
+        }
     }
 }
 
@@ -55,19 +73,19 @@ fun AppButtons(
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(bottom = 16.dp),
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
-        Button(onClick = onAboutClick) {
+        ElevatedButton(colors = ButtonDefaults.filledTonalButtonColors(), onClick = onAboutClick) {
             Text(text = "About")
         }
-        Button(onClick = onOfficerClick) {
+        ElevatedButton(colors = ButtonDefaults.filledTonalButtonColors(), onClick = onOfficerClick) {
             Text(text = "Officers")
         }
-        Button(onClick = onEventsClick){
+        ElevatedButton(colors = ButtonDefaults.filledTonalButtonColors(), onClick = onEventsClick){
             Text(text = "Events")
         }
-        Button(onClick = onAppsClick) {
+        ElevatedButton(colors = ButtonDefaults.filledTonalButtonColors(), onClick = onAppsClick) {
             Text(text = "Apps")
         }
     }
